6ac05efb5d2fff17f88117a9b81aa2ff00bd3846
Alexandr Burov <aiburov@rt-dc.ru>
2026-06-03 16:30:14 +0300

    CircleQueue implemented

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -11,41 +12,95 @@ import (
 
 type CircularQueue struct {
 	values []int
-	// need to implement
+	start  int
+	finish int
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{values: make([]int, size), start: -1, finish: -1}
+}
+
+func (q *CircularQueue) RemoveEdges() {
+	q.start = -1
+	q.finish = -1
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+	if q.Empty() {
+		q.finish = 0
+		q.start = 0
+		q.values[q.finish] = value
+		return true
+	}
+
+	q.finish = q.Next(q.finish)
+	q.values[q.finish] = value
+	return true
+}
+
+func (q *CircularQueue) Last() bool {
+	return q.start == q.finish
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.Empty() {
+		return false
+	}
+
+	q.values[q.start] = 0
+
+	if q.Last() {
+		q.RemoveEdges()
+	} else {
+		q.start = q.Next(q.start)
+	}
+
+	return true
+}
+
+func (q *CircularQueue) Next(idx int) int {
+	return (idx + 1) % len(q.values)
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	front := q.values[q.start]
+	if front == 0 {
+		return -1
+	}
+
+	return front
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	back := q.values[q.finish]
+
+	return back
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.start == -1 && q.finish == -1
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	return !q.Empty() && q.Next(q.finish) == q.start
 }
 
 func TestCircularQueue(t *testing.T) {
 	const queueSize = 3
 	queue := NewCircularQueue(queueSize)
 
+	fmt.Printf("INIT %+v\n", queue)
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
 
@@ -54,9 +109,14 @@ func TestCircularQueue(t *testing.T) {
 	assert.False(t, queue.Pop())
 
 	assert.True(t, queue.Push(1))
+	fmt.Printf("PUSH 1 %+v\n", queue)
+
 	assert.True(t, queue.Push(2))
+	fmt.Printf("PUSH 2 %+v\n", queue)
 	assert.True(t, queue.Push(3))
+	fmt.Printf("PUSH 3 %+v\n", queue)
 	assert.False(t, queue.Push(4))
+	fmt.Printf("PUSH 4 %+v\n", queue)
 
 	assert.True(t, reflect.DeepEqual([]int{1, 2, 3}, queue.values))
 
@@ -67,9 +127,12 @@ func TestCircularQueue(t *testing.T) {
 	assert.Equal(t, 3, queue.Back())
 
 	assert.True(t, queue.Pop())
+	fmt.Printf("POP %+v\n", queue)
+
 	assert.False(t, queue.Empty())
 	assert.False(t, queue.Full())
 	assert.True(t, queue.Push(4))
+	fmt.Printf("PUSH 4 %+v\n", queue)
 
 	assert.True(t, reflect.DeepEqual([]int{4, 2, 3}, queue.values))
 
@@ -77,9 +140,13 @@ func TestCircularQueue(t *testing.T) {
 	assert.Equal(t, 4, queue.Back())
 
 	assert.True(t, queue.Pop())
+	fmt.Printf("POP %+v\n", queue)
 	assert.True(t, queue.Pop())
+	fmt.Printf("POP %+v\n", queue)
 	assert.True(t, queue.Pop())
+	fmt.Printf("POP %+v\n", queue)
 	assert.False(t, queue.Pop())
+	fmt.Printf("POP %+v\n", queue)
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())

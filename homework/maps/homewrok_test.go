69362e8645a26dbc85eefd79513f6e171040acd8
Alexandr Burov <aiburov@rt-dc.ru>
2026-06-05 12:08:29 +0300

    OrderedMap implemented

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -9,37 +10,172 @@ import (
 
 // go test -v homework_test.go
 
+type Node struct {
+	key    int
+	value  int
+	parent *Node
+	left   *Node
+	right  *Node
+}
+
 type OrderedMap struct {
-	// need to implement
+	size int
+	root *Node
 }
 
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{
+		size: 0,
+		root: nil,
+	}
 }
 
+// func (m *OrderedMap) Insert(key, value int)          // добавить элемент в словарь
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	if m.size == 0 {
+		m.root = &Node{key: key, value: value}
+		m.size++
+		return
+	}
+
+	currentNode := m.root
+
+	fmt.Printf("INSERT %v %v\n", key, value)
+
+	for {
+		if currentNode.key == key {
+			return
+		} else if key > currentNode.key {
+			if currentNode.right == nil {
+				currentNode.right = &Node{key: key, value: value}
+				m.size++
+				return
+			}
+			currentNode = currentNode.right
+		} else if key < currentNode.key {
+			if currentNode.left == nil {
+				currentNode.left = &Node{key: key, value: value}
+				m.size++
+				return
+			}
+			currentNode = currentNode.left
+		}
+	}
 }
 
+func (m *OrderedMap) Find(key int) *Node {
+	if m.root == nil {
+		return nil
+	}
+
+	return FindNode(m.root, key)
+}
+
+func FindNode(node *Node, key int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	fmt.Printf("FIND NODE %v key %v\n", node, key)
+
+	if node.key == key {
+		return node
+	} else if key > node.key {
+		return FindNode(node.right, key)
+	} else {
+		return FindNode(node.left, key)
+	}
+}
+
+// func (m *OrderedMap) Erase(key int)                  // удалить элемент из словари
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	if m.root == nil {
+		return
+	}
+
+	m.root = m.EraseNode(m.root, key)
 }
 
+func (m *OrderedMap) EraseNode(node *Node, key int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if key > node.key {
+		node.right = m.EraseNode(node.right, key)
+		return node
+	}
+
+	if key < node.key {
+		node.left = m.EraseNode(node.left, key)
+		return node
+	}
+
+	if node.left == nil && node.right == nil {
+		m.size--
+		return nil
+	}
+
+	if node.left == nil {
+		m.size--
+		return node.right
+	}
+
+	if node.right == nil {
+		m.size--
+		return node.left
+	}
+
+	minNode := Left(node.right)
+
+	node.key = minNode.key
+	node.value = minNode.value
+
+	node.right = m.EraseNode(node.right, minNode.key)
+	return node
+}
+
+// func (m *OrderedMap) Contains(key int) bool          // проверить существование элемента в словаре
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	return m.Find(key) != nil
 }
 
+// func (m *OrderedMap) Size() int                      // получить количество элементов в словаре
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.size
 }
 
+// func (m *OrderedMap) ForEach(action func(int, int))  // применить функцию к каждому элементу словаря от меньшего к большему
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	walk(m.root, action)
+}
+
+func walk(node *Node, action func(int, int)) {
+	if node == nil {
+		return // дошли до пустого места — возвращаемся
+	}
+
+	walk(node.left, action)
+	action(node.key, node.value)
+	walk(node.right, action)
+}
+
+func Left(currentNode *Node) *Node {
+	if currentNode.left == nil {
+		return currentNode
+	}
+
+	return Left(currentNode.left)
 }
 
 func TestCircularQueue(t *testing.T) {
 	data := NewOrderedMap()
 	assert.Zero(t, data.Size())
+
+	// 		10
+	// 	5	  	 15
+	// 2		4 12
+	// 			14
 
 	data.Insert(10, 10)
 	data.Insert(5, 5)
@@ -57,7 +193,9 @@ func TestCircularQueue(t *testing.T) {
 
 	var keys []int
 	expectedKeys := []int{2, 4, 5, 10, 12, 14, 15}
-	data.ForEach(func(key, _ int) {
+	data.ForEach(func(key, v int) {
+		fmt.Printf("NODE %v %v\n", key, v)
+
 		keys = append(keys, key)
 	})
 
@@ -78,6 +216,8 @@ func TestCircularQueue(t *testing.T) {
 	data.ForEach(func(key, _ int) {
 		keys = append(keys, key)
 	})
+
+	fmt.Printf("%v %v\n", keys, expectedKeys)
 
 	assert.True(t, reflect.DeepEqual(expectedKeys, keys))
 }
